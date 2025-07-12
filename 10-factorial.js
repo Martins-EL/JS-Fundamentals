d3b24da14cdcbe91b280f459a96099cd021597da
Martins Liyansan <liyansanm@gmail.com>
2025-07-12 13:43:20 +0100

    Add 10-factorial.js file

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+    if (isNaN(n) || n === 0 || n < 0) {
+    return 1;
+    }
+    return n * factorial(n -1);
+}
+const arg = process.argv[2]
+const num = parseInt(arg);
+
+const result = factorial(num);
+console.log(result);
