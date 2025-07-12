707cf75e4de65a009812b4e7156b1d3679dfa9fd
Martins Liyansan <liyansanm@gmail.com>
2025-07-12 13:12:01 +0100

    Add 8-square.js file

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+const arg = process.argv[2];
+const size = parseInt(arg);
+if (isNaN(size) || size < 1) {
+    console.log("Missing size");
+} 
+else {
+ const rowOFXs = 'X' .repeat(size);
+ for (let i = 0; i < size; i++) {
+    console.log(rowOFXs);
+ }
+}
+
